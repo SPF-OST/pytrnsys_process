04ac227227bcaa4a7d4471743365ea952232564b
Damian Birchler <damian.birchler@ost.ch>
2024-01-30 00:09:10 +0100

    Use more of the available screen space.

--- doc/conf.py
+++ doc/conf.py
@@ -35,8 +35,11 @@ trim_doctest_flags = True
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "alabaster"
+
+html_theme_options = {
+    "page_width": "95%",
+}

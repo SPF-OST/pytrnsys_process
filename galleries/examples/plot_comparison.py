a415873b4a026b07ccc3b4160466db1365f8e254
sebastian-swob <sebastian@swoboda.ch>
2025-03-06 09:54:43 +0100

    removed large files

--- galleries/examples/plot_comparison.py
+++ galleries/examples/plot_comparison.py
@@ -20,7 +20,7 @@ def main():
     ###############################################
 
     simulations_data = api.load_simulations_data_from_pickle(
-        _pl.Path("../example_data/small/simulations_data.pkl")
+        _pl.Path("../example_data/small/simulations_data_reduced.pkl")
     )
 
     for sim_name, sim in simulations_data.simulations.items():

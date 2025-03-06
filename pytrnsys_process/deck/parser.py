0eb12f66524d7771b0244c349da980df94243577
sebastian-swob <sebastian@swoboda.ch>
2025-03-06 17:07:20 +0100

    disabled test and checks for main branch added linux test and check for release workflow minor changes to docstrings

--- pytrnsys_process/deck/parser.py
+++ pytrnsys_process/deck/parser.py
@@ -12,5 +12,20 @@ def _create_parser() -> _lark.Lark:
 
 
 def parse_dck(ddck_content: str) -> _lark.Tree:
+    """
+    Parse the provided dck content string and generate a tree structure using the Lark parser.
+
+    The function utilizes an internal parser to interpret the given dck_content and produce
+    a parsed tree object. It requires the content to be in a format understood by the parser.
+
+    Args:
+        ddck_content (str): The string content of the dck file to be parsed.
+
+    Returns:
+        _lark.Tree: The parsed tree representation of the provided dck content.
+
+    Raises:
+        Any exceptions raised by the underlying parser.
+    """
     tree = _create_parser().parse(ddck_content)
     return tree
